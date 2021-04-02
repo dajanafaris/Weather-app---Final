4fc542140cd9eb467fcb17e5201b6969df395723
Dajana <djnfrs255@gmail.com>
2021-04-02 19:02:32 +0200

    display date function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
+function displayDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
 
 function displayTemperature(response){
 let temperatureElement = document.querySelector("#temperature");
@@ -6,6 +29,10 @@ let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+
+
 celsiusTemperature = response.data.main.temp;
 
 
@@ -14,6 +41,7 @@ cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = response.data.wind.speed;
+dateElement = displayDate(currentTime);
 }
 
 function search (city) {
