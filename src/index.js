d8b6ad51e7f9795e626fc6e7149c67e6c5f1393d
Dajana <djnfrs255@gmail.com>
2021-04-02 11:44:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 
 
-
+function displayTemperature(response){
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 
+celsiusTemperature = response.data.main.temp;
 
+
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+cityElement.innerHTML = response.data.name;
+
+}
+
+function search (city) {
 let apiKey="6d15a99ffb8493d1efc42cac1693ceeb";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
-
+}
 
