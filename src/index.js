0ba1e14e2d7111b8e54bb0629d82219433282aef
Dajana <djnfrs255@gmail.com>
2021-04-02 18:44:26 +0200

    js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@
 function displayTemperature(response){
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
-
+let descriptionElement = document.querySelector("description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
 celsiusTemperature = response.data.main.temp;
 
 
 temperatureElement.innerHTML = Math.round(celsiusTemperature);
 cityElement.innerHTML = response.data.name;
-
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = response.data.wind.speed;
 }
 
 function search (city) {
