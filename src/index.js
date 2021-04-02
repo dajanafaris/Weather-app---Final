b9d90f666611ccbe68a595ef453bb5d3a0a4cb75
Dajana <djnfrs255@gmail.com>
2021-04-02 11:17:37 +0200

    apiKey

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+
+
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+
+
+let apiKey="6d15a99ffb8493d1efc42cac1693ceeb";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
+
+
