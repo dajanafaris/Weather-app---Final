9294fe663fb68d7bd57681aa5052251d483a32b4
Dajana <djnfrs255@gmail.com>
2021-04-17 19:49:08 +0200

    css final touch

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="42"
+          width="60"
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
